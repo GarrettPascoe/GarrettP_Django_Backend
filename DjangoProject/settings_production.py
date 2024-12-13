678ab2ee6afcc75be6f19c74773d277776fd7e45
Garrett Pascoe <garrettp523@gmail.com>
2024-12-13 15:48:48 -0500

    remove static storage

--- DjangoProject/settings_production.py
+++ DjangoProject/settings_production.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 STORAGES = {
     "default": {"django.core.files.storage.FileSystemStorage",},
     
-    "staticfiles": {'django.contrib.staticfiles.storage.StaticFilesStorage'},
+    #"staticfiles": {'django.contrib.staticfiles.storage.StaticFilesStorage'},
 }
 
 
@@ -112,7 +112,7 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = BASE_DIR/'staticfiles'
+#STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
